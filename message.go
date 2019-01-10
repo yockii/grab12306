e19c949ae3098f2c3bc3f0a7ef7796fee74ccf8c
徐昱琪 <yockii@xuyuqideMacBook-Pro.local>
2019-01-10 18:10:17 +0800

    go后段vue界面整合

---
+++ message.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/asticode/go-astilectron"
+	"github.com/asticode/go-astilectron-bootstrap"
+)
+
+
+
+func handleMessages(w *astilectron.Window, m bootstrap.MessageIn) (payload interface{}, err error) {
+	switch m.Name {
+	case "explore":
+			
+	}
+	return
+}
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App.vue'
+import router from './router'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
